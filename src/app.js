720e0abf5f542495c461581a1ce730ed893c5ace
Jatin-Mankhand <jatin.mankhand@newtonschool.co>
2023-02-01 17:38:06 +0530

    Done

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
-// Importing products from userDetails.json file
+// Importing products from products.json file
 const userDetails = JSON.parse(
     fs.readFileSync(`${__dirname}/data/userDetails.json`)
 );
@@ -10,10 +10,40 @@ const userDetails = JSON.parse(
 //Middlewares
 app.use(express.json())
 
-// POST endpoint for registering new user
+// Write POST endpoint for registering new user
 
 
 // GET endpoint for sending the details of users
+app.get('/api/v1/details', (req,res) => {
+    res.status(200).json({
+    status:'Success',
+    message:'Detail of users fetched successfully',
+    data:{
+        userDetails
+    }
+});
+});
 
+// GET endpoint for sending the products to client by id
+app.get('/api/v1/userdetails/:id',(req,res)=>{
+    let {id} = req.params;
+    id*=1;
+    const details = userDetails.find(details => details.id===id);
+    if(!details){
+        return res.status(404).send({
+            status:"failed",
+            message: "Product not found!"
+        });
+    }
+    else{
+    res.status(200).send({
+        status : 'success',
+        message : "Details of users fetched successfully",
+        data: {
+            details
+        }
+    });
+    }
+});
     
 module.exports = app;
