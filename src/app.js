d77ab7946e0f0163c82ed6c64f1d17a533101e83
Jatin-Mankhand <jatin.mankhand@newtonschool.co>
2023-02-01 14:03:09 +0530

    Done

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
-// Importing products from products.json file
+// Importing products from userDetails.json file
 const userDetails = JSON.parse(
     fs.readFileSync(`${__dirname}/data/userDetails.json`)
 );
@@ -11,34 +11,9 @@ const userDetails = JSON.parse(
 app.use(express.json())
 
 // POST endpoint for registering new user
-app.post('/api/v1/details', (req,res) => {
-    const newId = userDetails.length+1;
-    const newUser = Object.assign({ id: newId }, req.body);
-    userDetails.push(newUser);
-    fs.writeFile(
-        `${__dirname}/data/userDetails.json`,
-        JSON.stringify(userDetails), 
-        err => {
-            res.status(201).json({
-            status: 'Success',
-            message:'User registered successfully',
-            data:{
-                userDetails: newUser
-            }
-        });
 
-    });
-});
 
 // GET endpoint for sending the details of users
-app.get('/api/v1/details', (req,res) => {
-    res.status(200).json({
-    status:'Success',
-    message:'Detail of users fetched successfully',
-    data:{
-        userDetails
-    }
-});
-});
+
     
 module.exports = app;
